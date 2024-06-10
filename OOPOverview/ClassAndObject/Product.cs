8d1aba2f110f9620029bc4e9e0203d10cc514257
türkay ürkmez <turkay.urkmez@outlook.com>
2024-06-10 11:08:52 +0300

    starting encapsulation

---
+++ OOPOverview/ClassAndObject/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    internal class Product
+    {
+        public string Name;
+        public decimal Price;
+    }
+}
--- OOPOverview/ClassAndObject/Program.cs
+++ OOPOverview/ClassAndObject/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using ClassAndObject;
+
+Product gozluk = new Product();
+gozluk.Name = "Rayban";
+gozluk.Price = 5000;
+
+Product sapka = new Product();
+sapka.Name = "Tenisçi şapkası";
+
+Console.WriteLine(gozluk.Name);
